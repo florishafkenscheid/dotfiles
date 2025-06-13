0e3481cca620125bf23747b43c93288c42774752
Floris Hafkenscheid <floris@hafkenscheid.com>
2025-06-13 03:47:53 +0200

    feat: Freeze screen and hide cursor when taking a screenshot

--- config/hypr/scripts/screenshot.sh
+++ config/hypr/scripts/screenshot.sh
@@ -3,4 +3,4 @@ set -e # Exit safely when slurp is cancelled
 
 screenshot_dir="$HOME/Desktop/Screenshots"
 
-grim -g "$(slurp)" - | tee $screenshot_dir/$(date +'%s_grim.png') | wl-copy
+wayfreeze --hide-cursor --after-freeze-cmd 'grim -g "$(slurp)" - | tee '"$screenshot_dir/"'$(date +'%s_grim.png') | wl-copy; killall wayfreeze'
