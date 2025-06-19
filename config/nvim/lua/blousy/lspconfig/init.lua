3313145563f0c3de1aeb711454e89b001c32b9db
Floris Hafkenscheid <floris@hafkenscheid.com>
2025-06-19 09:11:46 +0200

    feat: Autocmd for rust formatting

--- config/nvim/lua/blousy/lspconfig/init.lua
+++ config/nvim/lua/blousy/lspconfig/init.lua
@@ -49,3 +49,10 @@ return {
         end
     end
 }
+
+vim.api.nvim_create_autocmd('BufWritePre', {
+  pattern = '*.rs',
+  callback = function()
+    vim.lsp.buf.format({ async = true })
+  end,
+})
