8e8830542dd32cc12606dc26e0388ecbfe38ccf5
Floris Hafkenscheid <floris@hafkenscheid.com>
2025-07-09 02:10:34 +0200

    refactor(media): Change to use spotify instead of spotify_player

--- config/hypr/scripts/media-control.sh
+++ config/hypr/scripts/media-control.sh
@@ -2,8 +2,8 @@
 
 ACTION=$1
 
-if playerctl -l | grep -q "spotify_player"; then
-    playerctl -p spotify_player "$ACTION"
+if playerctl -l | grep -q "spotify"; then
+    playerctl -p spotify "$ACTION"
 else
     playerctl "$ACTION"
 fi
